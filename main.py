ecf03357331ee4d1ea30b5c6ee5ce72961e6c816
Jayarch97 <jayquintanilla2@gmail.com>
2025-05-08 01:55:07 +0000

    adding python code to test at home

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
-import urllib.request
-import urllib.parse
-import json
 import google.oauth2.id_token
+import google.auth.transport.requests
+import jwt  # Added import for jwt
+import logging  # Added import for logging
+import requests # Added import for requests
 
 
-def make_authorized_get_request(endpoint, audience):
+def call_predictions(arr_dict, api_host, model_endpoint):
 
-    data = urllib.parse.urlencode({'key': 'This is mock data that will be sent'})
-    data = data.encode('ascii')
-
-    # uses the urlib library to get the endpoint
-    req = urllib.request.Request(endpoint, data=data)
-
-
-
-    # uses the authentication library to get the credintials. 
     auth_req = google.auth.transport.requests.Request()
-    id_token = google.oauth2.id_token.fetch_id_token(auth_req, audience)
-
-    req.add_header("Authorization", f"Bearer {id_token}")
-    response = urllib.request.urlopen(req)
-    print(response.read())
-    return response.read()
+    id_token = google.oauth2.id_token.fetch_id_token(auth_req, api_host)
 
 
-#testing the function with the endpoint and audiance.
-make_authorized_get_request('https://python-target-974768286444.us-central1.run.app/hello' , 'https://python-target-974768286444.us-central1.run.app')
+    # Decode the ID token
+    decoded_token = jwt.decode(id_token, options={"verify_signature": False})
+
+
+    # Print the decoded token
+    logging.info(f'decoded_token: {decoded_token}')
+
+    headers = {'Authorization': 'Bearer {}'.format(id_token)}
+
+
+    response = requests.post(api_host + "/" + model_endpoint, json=arr_dict,
+    headers=headers, verify=False)
+
+    try:
+        preds = response.json()
+        print(f"Predictions: {preds}")
+    except requests.exceptions.JSONDecodeError as e:
+        print(f"Error decoding JSON for predictions: {e}")
+        preds = None
+
+
+
+    return preds
+
+call_predictions({'key1': 'value10', 'key2': 'value20'} , 'https://python-target-974768286444.us-central1.run.app', 'hello'  )
